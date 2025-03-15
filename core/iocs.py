85bb0f7751fde865ba70d291450bb5ad4ab53de9
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-15 16:25:00 +0530

    new: added filters to the Ioc get endpoint

--- core/iocs.py
+++ core/iocs.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from typing import Dict, List
 
 from enums import IOCType
+from models import IOCFinding
 from database import AppDB
 from utils import mongo_serializer
 
@@ -8,11 +10,15 @@ def get_iocs(
     skip: int,
     limit: int,
     type_: IOCType,
+    filters: Dict[str, List],
     date_from: datetime = None,
     date_to: datetime = None,
 ):
     pipeline = []
     pipeline.insert(0, {"$match": {"ioc_types": type_}})
+    if filters:
+        if "ioc" in filters:
+            pipeline.append({"$match": {f"keys.{type_.value}": {"$in": filters["ioc"]}}})
     if date_from and date_to:
         pipeline.append({"$match": {"meta.date": {"$gte": date_from, "$lte": date_to}}})
     elif date_to:
@@ -23,6 +29,6 @@ def get_iocs(
     pipeline.extend([{"$skip": skip}, {"$limit": limit}])
     pipeline.append({"$project": {"_id": 0, "source_meta": 0}})
     res = AppDB().IOCs.aggregate(mongo_serializer(pipeline))
-    res = list(res)
+    res = list(map(lambda finding: IOCFinding(**finding), res))
     return res
-    
+
