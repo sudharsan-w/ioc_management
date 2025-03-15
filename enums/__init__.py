c331d21138581cc4432e8431386c784c6e8c23ba
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-15 10:52:44 +0530

    new: module to manage all enums

---
+++ enums/__init__.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class IOCType(Enum):
+    ipv4 = "IPV4"
+    ipv6 = "IPV6"
+    domain = "DOMAIN"
+    md5_hash = "MD5_HASH"
+    sha_hash = "SHA_HASH"
+    bitcoin_addr = "BITCOIN_ADDRESS"
+    email = "EMAIL"
+    filename = "FILENAME"
+    cve = "CVE"
+
+
+class SourceType(Enum):
+    files = "FILES"
+    misp = "MISP"
+    storage_bucket = "STORAGE_BUCKET"
+
