6440d16e7828d8c7d0afddcbb9d2e7c67137ab05
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-19 16:30:27 +0530

    update: 1) added api prefix to the application 2) updated the endpoints to support sort, pagination

--- http_api/app.py
+++ http_api/app.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, Security, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, APIRouter, Depends, Security
 from fastapi.security import APIKeyHeader
 from datetime import datetime
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 from enums import IOCType
 from utils import json_serializer
 from core import iocs, geo, asn, client
+from models import SortOrder
 from globals_ import env
 
-http_api = FastAPI()
+http_api = FastAPI(docs_url=f"{env.API_PREFIX}/docs")
+
+router = APIRouter(prefix=env.API_PREFIX)
 
 security = APIKeyHeader(name="X-API-Key")
 
@@ -25,20 +28,24 @@ def api_key_auth():
     return check_key
 
 
-@http_api.post("/v1/get/iocs", dependencies=[Depends(api_key_auth())])
+@router.post("/v1/get/iocs", dependencies=[Depends(api_key_auth())])
 def _get_iocs(
     page_no: int,
     per_page: int,
     type_: IOCType,
     filters: Dict[str, List] = {},
+    sort_by: Union[str, None] = None,
+    sort_order: SortOrder = "asc",
     date_from: datetime = None,
     date_to: datetime = None,
 ):
     return json_serializer(
         iocs.get_iocs(
-            skip=(page_no * per_page),
+            page=page_no,
             limit=per_page,
             filters=filters,
+            sort_by=sort_by,
+            sort_order=sort_order,
             type_=type_,
             date_from=date_from,
             date_to=date_to,
@@ -46,15 +53,19 @@ def _get_iocs(
     )
 
 
-@http_api.get("/v1/get/location", dependencies=[Depends(api_key_auth())])
+@router.get("/v1/get/location", dependencies=[Depends(api_key_auth())])
 def _get_location(ip: str):
     return json_serializer(geo.get_location(ip))
 
-@http_api.get("/v1/get/asn", dependencies=[Depends(api_key_auth())])
+
+@router.get("/v1/get/asn", dependencies=[Depends(api_key_auth())])
 def _get_asn(ip: str):
     return json_serializer(asn.get_asn_info(ip))
 
 
-@http_api.get("/v1/get/entity_info", dependencies=[Depends(api_key_auth())])
+@router.get("/v1/get/entity_info", dependencies=[Depends(api_key_auth())])
 def _entity_info(type_: IOCType, val: Any):
     return json_serializer(client.enrich_ioc(type_=type_, ioc=val))
+
+
+http_api.include_router(router)
