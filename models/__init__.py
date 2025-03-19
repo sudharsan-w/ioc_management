5d37e192f2f9713047ead5fad2c30e85fd1567e0
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-19 16:23:36 +0530

    update: created custom model by extending pydantic model to use throughout the application

--- models/__init__.py
+++ models/__init__.py
@@ -1,42 +1,72 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
-from typing import List, Dict, Any, Union
+from typing import List, Dict, Any, Union, Mapping, Literal
 
 from enums import IOCType, SourceType, Lang
 
+SortOrder = Literal["asc", "desc"]
 
-class SourceRef(BaseModel):
+
+class Model(BaseModel):
+
+    def __init__(self, data=None, **kwargs):
+        if isinstance(data, Mapping):
+            kwargs = {**data, **kwargs}
+        super().__init__(**kwargs)
+
+    def keys(self):
+        return list(self)
+
+    def __getitem__(self, key):
+        if key not in self.model_fields:
+            e = f"{key} not found"
+            raise KeyError(e)
+        return getattr(self, key)
+
+    def __iter__(self):
+        return iter(self.model_fields)
+
+    def __len__(self):
+        return len(self.model_fields)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self})"
+
+    model_config = {"populate_by_name": True}
+
+
+class SourceRef(Model):
     key: str
     type: SourceType
 
 
-class IOCFinding(BaseModel):
+class IOCFinding(Model):
     id: str
     source: str
     source_ref: SourceRef
     ioc_types: List[IOCType]
-    keys: Dict[IOCType, Union[Any, List[Any]]]
+    keys_: Dict[IOCType, Union[Any, List[Any]]] = Field(..., alias="keys")
     meta: Dict
     source_meta: Dict
     created_at: datetime
 
 
-class GeoLocation(BaseModel):
+class GeoLocation(Model):
 
-    class Cords(BaseModel):
+    class Cords(Model):
         latitude: float
         longitude: float
 
-    class Country(BaseModel):
+    class Country(Model):
         names: Dict[Union[Lang, str], str]
 
-    class Continent(BaseModel):
+    class Continent(Model):
         names: Dict[Union[Lang, str], str]
 
-    class City(BaseModel):
+    class City(Model):
         names: Dict[Union[Lang, str], str]
 
-    class Subdivision(BaseModel):
+    class Subdivision(Model):
         names: Dict[Union[Lang, str], str]
 
     ipv4: str
@@ -45,3 +75,16 @@ class GeoLocation(BaseModel):
     continent: Continent
     city: City
     subdivisions: List[Subdivision] = []
+
+
+class ASN(Model):
+    asn: str
+    organization_name: str
+    domain_name: str
+    entity_type: str
+    tor: bool
+    proxy: bool
+    vpn: bool
+    hosting: bool
+    relay: bool
+    service: bool
