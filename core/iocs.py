5ec3d82ca30103c28a3e940f2e99f4977a63ef8d
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-19 16:20:37 +0530

    update: added sort and pagination to the get_iocs module

--- core/iocs.py
+++ core/iocs.py
@@ -1,24 +1,31 @@
+import math
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from enums import IOCType
-from models import IOCFinding
+from models import IOCFinding, SortOrder
 from database import AppDB
 from utils import mongo_serializer
 
+
 def get_iocs(
-    skip: int,
+    page: int,
     limit: int,
     type_: IOCType,
     filters: Dict[str, List],
+    sort_by: Union[str, None] = None,
+    sort_order: SortOrder = "asc",
     date_from: datetime = None,
     date_to: datetime = None,
 ):
     pipeline = []
     pipeline.insert(0, {"$match": {"ioc_types": type_}})
+    pipeline.append({"$unwind": f"$keys.{type_.value}"})
     if filters:
         if "ioc" in filters:
-            pipeline.append({"$match": {f"keys.{type_.value}": {"$in": filters["ioc"]}}})
+            pipeline.append(
+                {"$match": {f"keys.{type_.value}": {"$in": filters["ioc"]}}}
+            )
     if date_from and date_to:
         pipeline.append({"$match": {"meta.date": {"$gte": date_from, "$lte": date_to}}})
     elif date_to:
@@ -26,10 +33,43 @@ def get_iocs(
     elif date_from:
         pipeline.append({"$match": {"meta.date": {"$lte": date_to}}})
     pipeline.append({"$sort": {"_id": -1}})
-    pipeline.extend([{"$skip": skip}, {"$limit": limit}])
     pipeline.append({"$project": {"_id": 0, "source_meta": 0}})
+    if sort_by:
+        sort = [{"$sort": {sort_by: 1 if sort_order == "asc" else -1}}]
+    else:
+        sort = []
+    pipeline.append(
+        {
+            "$facet": {
+                "agg": [{"$count": "count"}],
+                "paginated": [*sort, {"$skip": (page - 1) * limit}, {"$limit": limit}],
+            }
+        }
+    )
     res = AppDB().IOCs.aggregate(mongo_serializer(pipeline))
     res = list(res)
+    if len(res) == 0 or len(res[0]["agg"]) == 0:
+        total_results = 0
+        total_pages = 0
+        page_no = 0
+        has_prev_page = False
+        has_next_page = False
+        data = []
+    else:
+        total_results = res[0]["agg"][0]["count"]
+        total_pages = math.ceil(total_results // limit)
+        page_no = page
+        has_prev_page = page_no > 1
+        has_next_page = page_no < total_pages
+        data = res[0]["paginated"]
+    # data = list(map(IOCFinding, data))
     # res = list(map(lambda finding: IOCFinding(**finding), res))
-    return res
-
+    return {
+        "data": data,
+        "total_results": total_results,
+        "total_pages": total_pages,
+        "page_no": page_no,
+        "per_page": limit,
+        "has_next_page": has_next_page,
+        "has_prev_page": has_prev_page,
+    }
