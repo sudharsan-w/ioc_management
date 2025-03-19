d4550a3a8807dd39eb78a43fda6c2fbf5211cae3
sudharsan-w <sudharsan@saptanglabs.com>
2025-03-19 16:30:57 +0530

    update: ASN data model

--- core/asn.py
+++ core/asn.py
@@ -1,4 +1,89 @@
 from database import AppDB
+from models import ASN
+
+ASN_PROJECTION = lambda: [
+    {
+        "$project": {
+            "_id": 0,
+            "asn": "$asn",
+            "organization_name": "$name",
+            "domain_name": "$domain",
+            "entity_type": "$type",
+            "tor": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.tor", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+            "proxy": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.proxy", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+            "vpn": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.vpn", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+            "hosting": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.hosting", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+            "relay": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.relay", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+            "service": {
+                "$cond": {
+                    "if": {
+                        "$and": [
+                            {"$ifNull": ["$privacy.service", False]},
+                            {"$ne": ["$privacy.tor", ""]},
+                        ]
+                    },
+                    "then": True,
+                    "else": False,
+                }
+            },
+        }
+    }
+]
 
 
 def get_asn_info(ip: str):
@@ -6,90 +91,7 @@ def get_asn_info(ip: str):
     octects_copy = [*octects]
     octects_copy[2] = "0"
     octects_copy[3] = "0"
-    pipeline = [
-        {"$match": {"ipv4": ".".join(octects_copy)}},
-        {
-            "$project": {
-                "_id": 0,
-                "asn": "$asn",
-                "organization_name": "$name",
-                "domain_name": "$domain",
-                "entity_type": "$type",
-                "tor": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.tor", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-                "proxy": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.proxy", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-                "vpn": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.vpn", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-                "hosting": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.hosting", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-                "relay": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.relay", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-                "service": {
-                    "$cond": {
-                        "if": {
-                            "$and": [
-                                {"$ifNull": ["$privacy.service", False]},
-                                {"$ne": ["$privacy.tor", ""]},
-                            ]
-                        },
-                        "then": True,
-                        "else": False,
-                    }
-                },
-            }
-        },
-    ]
+    pipeline = [{"$match": {"ipv4": ".".join(octects_copy)}}, *ASN_PROJECTION()]
     res = AppDB().ASNRecords.aggregate(pipeline)
-    res = list(res)
+    res = list(map(ASN, res))
     return None if len(res) == 0 else res[0]
